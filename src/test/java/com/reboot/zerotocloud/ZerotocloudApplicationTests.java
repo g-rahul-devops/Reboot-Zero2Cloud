bf0e3c208573ca83fd6d1ba272911af92c5ab033
jayasurya <jayasuryamathavan@gmail.com>
2025-07-17 12:10:22 +0530

    Initial commit

---
+++ src/test/java/com/reboot/zerotocloud/ZerotocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot.zerotocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerotocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
